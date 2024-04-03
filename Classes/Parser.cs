a554d56aed9846b36be40127557b101036d3d908
Fady Damak <fadi.damak@hotmail.fr>
2024-04-03 10:47:12 +0200

    ye

--- Classes/Parser.cs
+++ Classes/Parser.cs
@@ -10,19 +10,18 @@ namespace Car_Service_App
     public class Parser
     {
         private List<Work> works = new List<Work>();
-        public Work Parse(string row) 
+        public Work Parse(string line) 
         {
-            string line;
             string[] data;
 
             
                
-                {data = line.Split(';');
-                Work addedWork = new Work(data[0], Int32.Parse(data[1]), Int32.Parse(data[2]));
-                works.Add(addedWork);
+            data = line.Split(';');
+            Work addedWork = new Work(data[0], Int32.Parse(data[1]), Int32.Parse(data[2]));
+            work.Add(addedWork);
             
             
-            return works;
+            return work;
         }
 
     }
